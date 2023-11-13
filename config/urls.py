ece328c48630bd1b67c9e7be3de19af72753b053
Derek Enos <derek.enos@gmail.com>
2023-11-13 12:19:12 -0500

    backup - imported ARCH CSS files and modified base template to mostly match that of ARCH

--- config/urls.py
+++ config/urls.py
@@ -24,8 +24,8 @@ from keystone import views
 from keystone.api import private_api, public_api
 
 urlpatterns = [
-    path("accounts/login/", auth_views.LoginView.as_view(), name="login"),
-    path("accounts/logout/", auth_views.LogoutView.as_view(), name="logout"),
+    path("login/", auth_views.LoginView.as_view(), name="login"),
+    path("logout/", auth_views.LogoutView.as_view(), name="logout"),
     path(
         "accounts/password_change/",
         auth_views.PasswordChangeView.as_view(),
