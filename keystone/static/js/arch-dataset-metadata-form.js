1df86474d393c69418ba05a189dc824d6f0db9e5
Derek Enos <derek.enos@gmail.com>
2023-11-14 14:26:55 -0500

    backup - added commit has to static url, first working (less the API response) dashboard view

---
+++ keystone/static/js/arch-dataset-metadata-form.js
@@ -0,0 +1,2 @@
+import"./chunk-styles.js";export{A as ArchDatasetMetadataForm}from"./chunk-arch-dataset-metadata-form.js";import"./chunk-query-all.js";
+//# sourceMappingURL=arch-dataset-metadata-form.js.map
---
+++ keystone/static/js/arch-generate-dataset-form.js
@@ -0,0 +1,2 @@
+import"./chunk-styles.js";import"./chunk-state.js";import"./chunk-query-all.js";import"./chunk-arch-alert.js";export{A as ArchGenerateDatasetForm}from"./chunk-arch-generate-dataset-form.js";
+//# sourceMappingURL=arch-generate-dataset-form.js.map
